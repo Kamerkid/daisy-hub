4313779d9d5aad9af4ede6eb327d614d1af3f1ad
kamerkid <aliehdaisy2002@gmail.com>
2025-05-21 23:16:07 +0200

    05-connect-to-mongodb-and-seed-products

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatNumberWithDecimal = (num: number): string => {
+  const [int, decimal] = num.toString().split('.')
+  return decimal? `${int}.${decimal.padEnd(2, '0')}` : int
+}
+
+//PROMPT: [ChatGTP] create toSlug ts arrow function 
+// that convert text to lowercase,
+//  remove non-word, non-whitespace characters, non-hyphen characters, replace with whitespace, trim leading hyphens and trim trailing hyphens
+
+export const toSlug = (text: string): string => 
+   text
+    .toLowerCase()
+    .replace(/[^a-z0-9\s-]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/^-+|-+$/g, '')
